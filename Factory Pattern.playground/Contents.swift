365457f4229b6cb787ae3212995bb9efc8296b81
Piyush <12435216+piyush23dez@users.noreply.github.com>
2019-12-21 23:20:10 -0800

    Update Contents.swift

--- Factory Pattern.playground/Contents.swift
+++ Factory Pattern.playground/Contents.swift
@@ -1,119 +1,56 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+//MARK: FACTORY PATTERN
 
-protocol JobSeekerProtocol {
+protocol JobPortal {
     func sendJobEmail() -> String
 }
 
-struct Contact {
-    
-    var name: String
-    var email: String
-    var salary: Double
-    var job: Job
-    
-    enum Job {
-        case iOS
-        case android
-        case java
-        case windows
-    }
-}
-
-struct iOSJobSeeker: JobSeekerProtocol {
-    
-    var contact: Contact
-    
-    init(contact: Contact){
-        self.contact = contact
-    }
-    
-    func sendJobEmail() -> String {
-        return "Dear \(contact.name), Send me your resume today! I have a job opportunity that can pay you \(contact.salary)."
-    }
-}
-
-
-struct AndroidJobSeeker: JobSeekerProtocol {
-    
-    var contact: Contact
-    
-    init(contact: Contact){
-        self.contact = contact
-    }
-    
-    func sendJobEmail() -> String {
-        return "Dear \(contact.name), Send me your resume today! I have a job opportunity that can pay you \(contact.salary)."
-    }
-}
-
-
-struct JavaJobSeeker: JobSeekerProtocol {
-    
-    var contact: Contact
-    
-    init(contact: Contact){
-        self.contact = contact
-    }
-    
-    func sendJobEmail() -> String {
-        return "Dear \(contact.name), Send me your resume today! I have a job opportunity that can pay you \(contact.salary)."
-    }
-}
-
-struct WindowsJobSeeker: JobSeekerProtocol {
-    
-    var contact: Contact
-    
-    init(contact: Contact){
-        self.contact = contact
-    }
-    
-    func sendJobEmail() -> String {
-        return "Dear \(contact.name), Send me your resume today! I have a job opportunity that can pay you \(contact.salary)."
-    }
-}
-
-
-
-struct JobSeekerFactory {
-    
-    static func getJobSeeker(contact: Contact) -> JobSeekerProtocol {
-       
+struct JobPortalFactory {
+    func getJobPortal(for contact: Contact) -> JobPortal {
         switch contact.job {
         case .iOS:
-            return iOSJobSeeker(contact: contact)
-        case .android:
-            return AndroidJobSeeker(contact: contact)
-        case .java:
-            return JavaJobSeeker(contact: contact)
-        case .windows:
-            return WindowsJobSeeker(contact: contact)
+            return iOSJobPortal(contact)
+        case .Andriod:
+            return AndriodJobPortal(contact)
         }
     }
 }
 
-
-var contacts = [Contact]()
-
-contacts.append(Contact(name: "Jack", email: "jack@example.com", salary: 110000, job: .iOS))
-
-contacts.append(Contact(name: "Rock", email: "rock@example.com", salary: 120000, job: .android))
-
-contacts.append(Contact(name: "mark", email: "mark@example.com", salary: 130000, job: .java))
-
-
-for contact in contacts {
-    let client = JobSeekerFactory.getJobSeeker(contact: contact)
-    print (client.sendJobEmail())
+struct Contact {
+    let name: String
+    let email: String
+    let job: Job
+    enum Job {
+        case Andriod, iOS
+    }
 }
 
+struct iOSJobPortal: JobPortal {
+    private let contact: Contact
+    
+    init(_ contact: Contact) {
+        self.contact = contact
+    }
+    
+    func sendJobEmail() -> String {
+        return "Hi \(contact.name) iOS Developer"
+    }
+}
 
+struct AndriodJobPortal: JobPortal {
+    private let contact: Contact
+    
+    init(_ contact: Contact) {
+        self.contact = contact
+    }
+    
+    func sendJobEmail() -> String {
+        return "Hi \(contact.name) Andriod Developer"
+    }
+}
 
-
-
-
-
-
-
+let iOSContact = Contact(name: "Steve", email: "ABC", job: .iOS)
+let andriodContact = Contact(name: "Mark", email: "XYZ", job: .Andriod)
+let factory = JobPortalFactory()
+factory.getJobPortal(for: andriodContact).sendJobEmail()
+factory.getJobPortal(for: iOSContact).sendJobEmail()
